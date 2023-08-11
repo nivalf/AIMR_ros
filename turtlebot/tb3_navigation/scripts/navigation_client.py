aab0ea74151a9e596c7d0687fea63a4306d238b8
nivalf <flavinleejohn@gmail.com>
2023-08-11 16:28:42 +0100

    Navigation logic

---
+++ turtlebot/tb3_navigation/scripts/navigation_client.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import rospy
+import actionlib
+from navigation.msg import NavigateToAssemblyAction, NavigateToAssemblyGoal
+
+class NavigationClient:
+    """
+    NavigationClient class interacts with the navigation server to navigate the humanoid robot to the assembly system.
+    It sends a goal, receives continuous feedback on the progress, and retrieves the final result.
+    """
+
+    def __init__(self):
+        """
+        Initializes the NavigationClient and connects to the navigation server.
+        """
+        self.client = actionlib.SimpleActionClient('tb3_navigate_to_assembly', NavigateToAssemblyAction)
+        rospy.loginfo("Waiting for navigation server...")
+        self.client.wait_for_server()
+        rospy.loginfo("Navigation server started.")
+
+    def feedback_callback(self, feedback):
+        """
+        Callback function for receiving feedback from the navigation server.
+        Prints the status updates.
+        """
+        rospy.loginfo(f"Navigation Status: {feedback.status}")
+
+    def send_navigation_goal(self):
+        """
+        Sends the navigation goal to the server and handles feedback and result.
+        """
+        try:
+            goal = NavigateToAssemblyGoal()
+            # You can set any specific parameters related to the navigation goal here
+
+            self.client.send_goal(goal, feedback_cb=self.feedback_callback)
+            rospy.loginfo("Navigation Goal Sent.")
+
+            # Wait for the result (blocking call)
+            self.client.wait_for_result()
+
+            # Retrieve the result
+            result = self.client.get_result()
+
+            if result.success:
+                rospy.loginfo("Navigation completed successfully.")
+            else:
+                rospy.logerr(f"Navigation failed: {result.message}")
+
+        except Exception as e:
+            rospy.logerr(f"Error in sending navigation goal: {e}")
+
+if __name__ == '__main__':
+    rospy.init_node('navigation_client')
+    client = NavigationClient()
+    client.send_navigation_goal()
---
+++ turtlebot/tb3_navigation/scripts/navigation_server.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import rospy
+import actionlib
+from navigation.msg import NavigateToAssemblyAction, NavigateToAssemblyFeedback, NavigateToAssemblyResult
+from perception.msg import DetectionResult
+# Import the tb3 modules from tb3.py
+from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
+
+class NavigationServer:
+    """
+    NavigationServer class using Finite State Machine (FSM) to manage the navigation of the humanoid robot.
+    States:
+        SCANNING: Turning left, looking for the assembly system.
+        MOVING_FORWARD: Moving towards the assembly system.
+        STOPPED: Stopped.
+    """
+
+    SCANNING = 0
+    MOVING_FORWARD = 1
+    STOPPED = 2
+
+    def __init__(self):
+        """
+        Initializes the NavigationServer and sets up the FSM logic.
+        """
+        self.server = actionlib.SimpleActionServer('tb3_navigate_to_assembly', NavigateToAssemblyAction, self.execute_cb, False)
+        self.server.start()
+        self.state = self.SCANNING
+
+        self.detection_subscriber = rospy.Subscriber('/assembly_system_detector/result', DetectionResult, self.resultdetection_cb)
+        self.detection_count = 0
+
+        # pull in some useful publisher/subscriber functions from the tb3.py module:
+        self.vel_controller = Tb3Move()
+        self.tb3_odom = Tb3Odometry()
+        self.tb3_lidar = Tb3LaserScan()
+
+    def resultdetection_cb(self, msg):
+        """
+        Callback for assembly system detection. Updates FSM state based on detection status.
+        """
+        rospy.loginfo(f"Detection result: {msg}")
+        if msg.detected == True:
+            if self.state == self.SCANNING:
+                self.set_state(self.MOVING_FORWARD)
+            elif self.state == self.MOVING_FORWARD and msg.percentage_cover > 75:
+                self.set_state(self.STOPPED)
+            self.detection_count = 0
+        else:
+            self.detection_count += 1
+            if self.state == self.MOVING_FORWARD and self.detection_count >= 5:
+                self.set_state(self.SCANNING)
+
+    def execute_cb(self, goal):
+        """
+        Execution logic for the navigation action using FSM.
+        Implements navigation process and sends feedback.
+        """
+        feedback = NavigateToAssemblyFeedback()
+        result = NavigateToAssemblyResult()
+
+        self.set_state(self.SCANNING)
+
+        try:
+            while not self.server.is_preempt_requested() and not rospy.is_shutdown():
+                if self.state == self.SCANNING:
+                    feedback.status = "Scanning for assembly system."
+                    # publish a velocity command to make the robot scanning. velocity set in transition fn.
+                    self.vel_controller.publish()
+
+                elif self.state == self.MOVING_FORWARD:
+                    feedback.status = "Assembly system detected. Moving forward."
+                    # publish a velocity command to make the robot move. velocity set in transition fn.
+                    self.vel_controller.publish()
+
+                elif self.state == self.STOPPED:
+                    feedback.status = "Navigation stopped."
+                    # publish a velocity command to make the robot stop. velocity set in transition fn.
+                    self.vel_controller.publish()
+                    break
+
+                self.server.publish_feedback(feedback)
+                rospy.sleep(0.1) # Polling interval
+
+            result.success = True
+            result.message = "Navigation complete."
+            self.server.set_succeeded(result)
+
+        except Exception as e:
+            rospy.logerr(f"Error in navigation process: {e}")
+            result.success = False
+            result.message = "Navigation failed due to an error."
+            self.server.set_aborted(result)
+
+    def set_state(self, state):
+        """
+        Sets the state of the FSM.
+        Transition function: Add logic that should be 
+        executed only at state transitions here.
+        """
+        if state == self.SCANNING:
+            # set the robot's angular velocity...
+            self.vel_controller.set_move_cmd(0, 1.5)
+
+        elif state == self.MOVING_FORWARD:
+            self.vel_controller.set_move_cmd(0.2, 0)
+
+        elif state == self.STOPPED:
+            self.vel_controller.set_move_cmd(0, 0)
+
+        self.state = state
+
+if __name__ == '__main__':
+    rospy.init_node('navigation_server')
+    server = NavigationServer()
+    rospy.spin()
